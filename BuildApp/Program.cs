1000d82d8d310afd1557737d5106b1ab2ce98692
Fabio Dario <fdario@GCMLP.COM>
2018-12-13 09:57:02 -0200

    Adding sample project and azure-pipelines.yml

---
+++ BuildApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BuildApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Build Works!!!");
+        }
+    }
+}
